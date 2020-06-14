c127a613a416ddc7347683f6c3b2026f429ef8f2
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-06-14 12:16:23 -0700

    add user interface and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,7 @@ AddressBook.prototype.findContact = function(id) {
 }
 
 AddressBook.prototype.deleteContact = function(id) {
-    for (let i=0; i< this.contacts.length i++) {
+    for (let i=0; i< this.contacts.length; i++) {
         if (this.contacts[i]) {
             if (this.contacts[i].id === id) {
                 delete this.contacts[i];
@@ -47,3 +47,27 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
 }
+
+// User Interface Logic
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+    let contactsList = $("ul#contacts");
+    let htmlForContactInfo = "";
+    addressBookToDisplay.contacts.forEach(function(contact) {
+        htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>"
+    })
+    contactsList.html(htmlForContactInfo);
+}
+
+$(document).ready(function() {
+    $("form#new-contact").submit(function(event){
+        event.preventDefault();
+        const inputtedFirstName = $("input#new-first-name").val();
+        const inputtedLastName = $("input#new-last-name").val();
+        const inputtedPhoneNumber = $("input#new-phone-number").val();
+        let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+        addressBook.addContact(newContact);
+        displayContactDetails(addressBook);
+    })
+})
